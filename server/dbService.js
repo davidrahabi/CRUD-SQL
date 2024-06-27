2dc026e6c2215ce709eee284af2b9b5fe2cb8d43
David Rahabi <107571256+davidrahabi@users.noreply.github.com>
2024-06-27 17:41:15 -0400

    Update dbService.js

--- server/dbService.js
+++ server/dbService.js
@@ -61,6 +61,58 @@ class DbService {
             console.log(error);
         }
     }
+    async deleteRowById(id){
+        try{
+            id = parseInt(id, 10);
+            const response =  await new Promise((resolve, reject) =>{
+                const query = "DELETE FROM names WHERE id =  ?;";
+                connection.query(query, [id], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+    
+            });
+            return response === 1 ? true : false;
+        }catch{
+            console.log(error);
+            return false;
+        }
+    }
+    async updateNameById(id, name) {
+        try {
+            id = parseInt(id, 10); 
+            const response = await new Promise((resolve, reject) => {
+                const query = "UPDATE names SET name = ? WHERE id = ?";
+    
+                connection.query(query, [name, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+    
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async searchByName(name) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM names WHERE name = ?;";
+
+                connection.query(query, [name], (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 module.exports = DbService;
